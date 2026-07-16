a06207f14f183c136cb5c6d7a858209ea5c7ece8
robert wiggins <wigginsro11@gmail.com>
2026-07-16 14:58:02 -0400

    ssl reject unauthorized false, wrap dev env vars check in conditional

--- src/server.js
+++ src/server.js
@@ -4,17 +4,20 @@ const knex = require('knex');
 const app = require('./app');
 const { PORT } = require('./config');
 
-const requiredVariables = [
-  'MIGRATION_DB_HOST',
-  'MIGRATION_DB_PORT',
-  'MIGRATION_DB_NAME',
-  'MIGRATION_DB_USER',
-  'MIGRATION_DB_PASS',
-];
 
-for (const variable of requiredVariables) {
-  if (typeof process.env[variable] !== 'string' || !process.env[variable]) {
-    throw new Error(`Missing or invalid environment variable: ${variable}`);
+if (process.env.NODE_ENV === 'development') {
+  const requiredVariables = [
+    'MIGRATION_DB_HOST',
+    'MIGRATION_DB_PORT',
+    'MIGRATION_DB_NAME',
+    'MIGRATION_DB_USER',
+    'MIGRATION_DB_PASS',
+  ];
+  
+  for (const variable of requiredVariables) {
+    if (typeof process.env[variable] !== 'string' || !process.env[variable]) {
+      throw new Error(`Missing or invalid environment variable: ${variable}`);
+    }
   }
 }
 
